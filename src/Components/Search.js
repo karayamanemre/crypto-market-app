ef27ae0060b73afbe2d240c0c417fd2ffb2cd614
Emre Karayaman <karayaman.emre@outlook.com>
2022-11-22 01:10:24 +0300

    style changes

--- src/Components/Search.js
+++ src/Components/Search.js
@@ -6,12 +6,15 @@ import styles from './Styles/Search.module.css';
 const Search = () => {
   const dispatch = useDispatch();
   return (
+    <div className={styles.container}>
       <input
         type="text"
         placeholder="Search Cryptocurrency"
         onChange={(e) => filter(e, dispatch)}
         className={styles.search_input}
       />
+      <p>Data from <a href="https://www.coingecko.com/" target="_blank" rel="noreferrer">CoinGecko</a></p>
+    </div>
   );
 };
 
